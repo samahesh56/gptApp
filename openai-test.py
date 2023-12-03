45036476c1a48bb1729bc083a4d8e1304d31ea7f
samahesh56 <samahesh56@gmail.com>
2023-12-02 21:24:53 -0500

    Basic gpt reponses

--- openai-test.py
+++ openai-test.py
@@ -1,12 +1,25 @@
 from openai import OpenAI
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+def chat_gpt(prompt, model="gpt-3.5-turbo"):
+    messages=[
+        {"role": "system", "content": "You are an assistant, skilled in explaining and providing answers for any question given by a user."},
+        {"role": "user", "content": prompt}
   ]
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=0.7  
 )
+    
+    reply = response.choices[0].message.content
+    return reply
 
-print(completion.choices[0].message)
+while True:
+    user_input = input("Ask ChatGPT, enter 'exit' or 'quit' to stop: ")
+    if user_input.lower() in ['exit', 'quit']:
+        break
+
+    response = chat_gpt(user_input)
+
+    print("ChatGPT:", response)

9b11bffc43e735e87cf61a251ee8d60645a6e33f
samahesh56 <samahesh56@gmail.com>
2023-12-04 16:47:11 -0500

    prompt-info

--- conversation.py
+++ conversation.py
@@ -5,7 +5,7 @@ client = OpenAI()
 
 DEFAULT_CONVERSATION = {
     "messages": [
-        {"role": "system", "content": "You are an assistant providing help with programming"},
+        {"role": "system", "content": "You are an assistant providing help with programming"}, # This is the base conversation when program is rerun
         {"role": "user", "content": "What can you help me with today?"}
     ]
 }
@@ -27,11 +27,11 @@ def send_message(user_input, conversation_text):
 
 def chat_gpt(prompt, model):
     #improved_prompt = "I am working on a gpt-API script in python. I am using the GPT model to assist me with building and debugging my code. Provide me with guidance, suggestions, and any necessary code samples to help me resolve this issue? I would appreciate detailed explanations and examples to help me understand the solution better. Thank you!"
-    improved_prompt = "I am working on a 300-500 word essay. Provide me with guidance, suggestions, and any necessary help I require. Thank you!"
+    #improved_prompt = "I am working on a 300-500 word essay. Provide me with guidance, suggestions, and any necessary help I require. Thank you!"
 
     messages = [
         {"role": "system", "content": "You are an assistant providing help with any issues. "},
-        {"role": "user", "content": prompt + improved_prompt}
+        {"role": "user", "content": prompt }
     ]
     response = client.chat.completions.create(
         model=model,

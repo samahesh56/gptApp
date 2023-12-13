acd5941c80480aca0015765cd7cb38bc8971a397
samahesh56 <samahesh56@gmail.com>
2023-12-13 16:57:30 -0500

    settings menu doc

--- src/gui.py
+++ src/gui.py
@@ -10,7 +10,7 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
         self.filename = "data/conversation.json" #
         self.init_gui() # initializes gui functions 
 
-        if self.filename:
+        if self.filename: # loads the current conversation to the text window. (conversation.json)
             self.conversational_logic.load_conversation()
             self.update_conversation_text()
 
@@ -20,7 +20,7 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
         menu_bar = tk.Menu(self.parent) # a Tkinter Menu holds a dropdown of contents for ease of access. Pass the parent root (self.parent) heree
         self.parent.config(menu=menu_bar) # assigns the menu bar to the parent window(root). This sets the menu bar to the tkinter window.
 
-        # File bar 
+        # File Menu 
         file_menu = tk.Menu(menu_bar, tearoff=0) 
         file_menu.add_command(label="New Conversation", command=self.new_conversation)
         file_menu.add_command(label="Open Conversation", command=self.load_conversation)
@@ -34,11 +34,11 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
         menu_bar.add_command(label="ChatGPT Settings", command=self.open_settings_menu)
 
         # Main conversation text widget
-        self.scrollbar = tk.Scrollbar(self.parent, orient=tk.VERTICAL)
+        self.scrollbar = tk.Scrollbar(self.parent, orient=tk.VERTICAL) 
         self.conversation_text = tk.Text(self.parent, state='normal', wrap=tk.WORD, yscrollcommand=self.scrollbar.set)
-        self.scrollbar.config(command=self.conversation_text.yview)
+        self.scrollbar.config(command=self.conversation_text.yview) # provides scroll wheel for the conversation_text. 
 
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y) # pack scroll wheel inside the conversation text 
         self.conversation_text.pack(padx=5, pady=5, fill=tk.BOTH, expand=True)
     
         
@@ -111,34 +111,13 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
             messagebox.showinfo("Save", "The conversation has been saved.")
 
     def open_settings_menu(self):
-        settings_window = tk.Toplevel(self.parent)
-        settings_window.title('Settings')
+        settings_window = tk.Toplevel(self.parent) # This creates a sub-window to pop up. Attaches it to the main gui. 
+        settings_window.geometry('400x300')
+        settings_window.title('Settings') 
 
-        configs = self.conversational_logic.config
-        
-        # Example setting: Model selection
-        tk.Label(settings_window, text='Model:').grid(row=0, column=0)
-        model_var = tk.StringVar()
-        model_select = ttk.Combobox(settings_window, textvariable=model_var)
-        model_select['values'] = ('gpt-3.5-turbo', 'gpt-4-1106-preview', 'gpt-4')  # List your models here
-        model_select.grid(row=0, column=1)
-        model_var.set(configs.get('model'))
+        configs = self.conversational_logic.config # loads the current configs in the user's configs.json 
 
-        # Max Tokens
-        tk.Label(settings_window, text='Max Tokens:').grid(row=1, column=0)
-        max_tokens_var = tk.IntVar()
-        max_tokens_entry = tk.Entry(settings_window, textvariable=max_tokens_var)
-        max_tokens_entry.grid(row=1, column=1)
-        max_tokens_var.set(configs.get('max_tokens'))  # Set current max tokens
-
-        # System Message
-        tk.Label(settings_window, text='System Message:').grid(row=2, column=0)
-        system_message_var = tk.StringVar()
-        system_message_entry = tk.Entry(settings_window, textvariable=system_message_var)
-        system_message_entry.grid(row=2, column=1)
-        system_message_var.set(configs.get('system_message', ''))  # Set current system message
-
-         # Function to update config on 'apply'
+        # Function to update config on 'apply'
         def update_config():
             configs['model'] = model_var.get()
             configs['max_tokens'] = max_tokens_var.get()
@@ -150,13 +129,35 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
             with open('configs.json', 'w') as f:
                 json.dump(configs, f)
 
-            self.conversational_logic.update_settings(configs)
+            self.conversational_logic.update_settings(configs) # when apply is pressed, the changes made are applied in real-time by updating configs in conversation_logic. 
 
-            #settings_window.destroy()  Enable this if you want to close the settings menu after apply is pressed. Otherwise, add a pop-up or notification that says "Settings Changed"
+            settings_window.destroy()  #Disable this if you want to close the settings menu open after apply is presed. Otherwise, add a pop-up or notification that says "Settings Changed"
 
         # Apply button
-        apply_button = tk.Button(settings_window, text='Apply', command=update_config)
+        apply_button = tk.Button(settings_window, text='Apply', command=update_config) 
         apply_button.grid(row=4, column=0, columnspan=2, pady=10)
+        
+        # Model selection
+        tk.Label(settings_window, text='Model:').grid(row=0, column=0)
+        model_var = tk.StringVar() # creates a selection of String data to get/set. 
+        model_select = ttk.Combobox(settings_window, textvariable=model_var) # Dropdown menu for changing models. Text is held in model_var
+        model_select['values'] = ('gpt-3.5-turbo', 'gpt-4-1106-preview', 'gpt-4')  # Set available models here 
+        model_select.grid(row=0, column=1) # assigns it to column 1. 
+        model_var.set(configs.get('model'))
+
+        # Max Tokens
+        tk.Label(settings_window, text='Max Tokens:').grid(row=1, column=0)
+        max_tokens_var = tk.IntVar() # holds the max token entry as an int
+        max_tokens_entry = tk.Entry(settings_window, textvariable=max_tokens_var)
+        max_tokens_entry.grid(row=1, column=1)
+        max_tokens_var.set(configs.get('max_tokens'))  # Sets config max tokens
+
+        # System Message
+        tk.Label(settings_window, text='System Message:').grid(row=2, column=0)
+        system_message_var = tk.StringVar()
+        system_message_entry = tk.Entry(settings_window, textvariable=system_message_var)
+        system_message_entry.grid(row=2, column=1)
+        system_message_var.set(configs.get('system_message'))  # Set current system message
 
 
     def exit_application(self):

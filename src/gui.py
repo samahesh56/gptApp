bf77282479c031e9b0f766c287ec2100b301acd9
samahesh56 <samahesh56@gmail.com>
2023-12-15 16:27:05 -0500

    Squashed commit of the following:
    commit 3f586d85e2ba084584258578ddb863e11e6ea0e6
    Author: samahesh56 <samahesh56@gmail.com>
    Date:   Fri Dec 15 16:04:40 2023 -0500
    
        docstrings for gui, logic fixes, Error handling.
    
        Improved readability and documentation of the project.
        Improved gui settings
    
    commit bf1a6d3ccd2498796e32735c5c8eb69a4300a65c
    Author: samahesh <samahesh56@gmail.com>
    Date:   Fri Dec 15 03:03:39 2023 -0500
    
        Error Handling Basics
    
    commit 4b7a3a2617337e1b9d971eed33c9ead9fed0b9b6
    Author: samahesh <samahesh56@gmail.com>
    Date:   Thu Dec 14 22:56:59 2023 -0500
    
        changed api key logic to utilize config file.
    
        Implement api key logic to be entered through the GUI settings
    
    commit 14f875545cbb7da951c2cd2707ca5341188b21f0
    Author: samahesh56 <samahesh56@gmail.com>
    Date:   Thu Dec 14 22:34:37 2023 -0500
    
        reset conversations
    
    commit 9d5a20589099015dec8540c54e53debd1ddc8177
    Author: samahesh56 <samahesh56@gmail.com>
    Date:   Thu Dec 14 20:58:06 2023 -0500
    
        venv fixes
    
    commit 7a2a325dbb064a73bd84de0a33a004eaa1ddf914
    Author: samahesh <samahesh56@gmail.com>
    Date:   Thu Dec 14 18:04:30 2023 -0500
    
        updates
    
        .env and .configs creation. New dependencies
    
    commit cb5585818a9673c8b1dd27f006f93f479db4c677
    Author: samahesh56 <samahesh56@gmail.com>
    Date:   Thu Dec 14 14:25:16 2023 -0500
    
        default files script test
    
    commit a3c6d17098b73e0d9c350e7f15eb87747442b7d4
    Author: samahesh56 <samahesh56@gmail.com>
    Date:   Thu Dec 14 02:29:26 2023 -0500
    
        venv
    
    commit 8f6b4c07d3b01db686996e03e8772aaa64b3c4c9
    Author: samahesh56 <samahesh56@gmail.com>
    Date:   Wed Dec 13 22:10:53 2023 -0500
    
        No longer requires users to install config file

--- src/gui.py
+++ src/gui.py
@@ -2,13 +2,23 @@ import json, tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 from conversation_logic import ConversationLogic
 
-class Main(tk.Frame): # Packs the program in a tkinter frame 
-    def __init__(self, parent, conversation_logic, *args, **kwargs): #*args allows for more inputs into the frame, **kwargs for dictionary value/pairs. 
+class Main(tk.Frame): 
+    """A class that creates the main GUI frame for the ChatGPTApp"""
+
+    def __init__(self, parent, conversation_logic, *args, **kwargs): 
+        """Initializes the GUI frame
+
+        Args:
+        parent (tk.Tk or tk.Toplevel): The parent widget
+        conversation_logic (ConversationLogic): An instance of the ConversationLogic class
+        *args: Additional positional arguments
+        **kwargs: Additional keyword arguments """
+
         super().__init__(parent, **kwargs) 
-        self.parent = parent # Instantiates frame to parent widget  
-        self.conversational_logic = conversation_logic #Creates an instance of ConversationLogic functions
-        self.filename = "data/conversation.json" #
-        self.init_gui() # initializes gui functions 
+        self.parent = parent 
+        self.conversational_logic = conversation_logic 
+        self.filename = "data/conversation.json" 
+        self.init_gui()
 
         if self.filename: # loads the current conversation to the text window. (conversation.json)
             self.conversational_logic.load_conversation()
@@ -16,11 +26,15 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
 
 
     def init_gui(self):
+        """Initializes the graphical user interface (GUI) elements
+        
+        This includes the menu bar, conversation text widget, and toolbar """
+
         # Menu Bar
         menu_bar = tk.Menu(self.parent) # a Tkinter Menu holds a dropdown of contents for ease of access. Pass the parent root (self.parent) heree
         self.parent.config(menu=menu_bar) # assigns the menu bar to the parent window(root). This sets the menu bar to the tkinter window.
 
-        # File Menu 
+        # File Menu (Menu Toolbar)
         file_menu = tk.Menu(menu_bar, tearoff=0) 
         file_menu.add_command(label="New Conversation", command=self.new_conversation)
         file_menu.add_command(label="Open Conversation", command=self.load_conversation)
@@ -28,9 +42,10 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
         file_menu.add_separator()
         file_menu.add_command(label="Settings", command=self.open_settings_menu)
         file_menu.add_command(label="Exit", command=self.exit_application)
+        
+        # Menu Toolbar Commands
         menu_bar.add_cascade(label="File", menu=file_menu)
-
-        # Settings Menu 
+        menu_bar.add_command(label="Load", command=self.load_conversation)
         menu_bar.add_command(label="ChatGPT Settings", command=self.open_settings_menu)
 
         # Main conversation text widget
@@ -57,25 +72,42 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
 
 
     def on_send_button_click(self):
+        """Handles the action when the Send button is clicked.
+        
+        Processes user input and displays GPT response in the conversation text widget """
+
         user_input = self.user_input_entry.get() # takes in the user input 
         self.user_input_entry.delete(0, tk.END) 
-        gpt_response = self.conversational_logic.chat_gpt(user_input, model=self.conversational_logic.model, max_tokens=self.conversational_logic.max_tokens) # performs the API call (chat gpt function call, class conversation_logic)
-
-        self.conversation_text.insert(tk.END, f"User: {user_input}\n")
-        self.conversation_text.insert(tk.END, f"GPT: {gpt_response}\n\n")
-        self.conversation_text.see(tk.END)
-
+        gpt_response, error_response = self.conversational_logic.chat_gpt(user_input) # performs the API call (chat gpt function call, class conversation_logic)
+        
+        # Check if response is an error message
+        if gpt_response is not None:
+            # Updates conversation to the conversation_text field
+            self.conversation_text.insert(tk.END, f"User: {user_input}\n")
+            self.conversation_text.insert(tk.END, f"GPT: {gpt_response}\n\n")
+            self.conversation_text.see(tk.END)
+        else:
+            # Display the error message in the GUI
+            if "401" or "APIConnectionError" in error_response:
+                messagebox.showinfo("Authentication Error", "Invalid or expired API key. Please check your API key.")
+            
     def on_reset_button_click(self):
+        """Handles the action when the Reset Conversation button is clicked.
+
+        Resets the conversation in the conversation text widget (clears the conversation in the gui """
+
         self.conversational_logic.reset_conversation() # reset function call
         self.conversation_text.delete("1.0", tk.END)
 
-    def update_conversation_text(self, filename=None):
-        conversation = self.conversational_logic.load_conversation(filename)  # Modify this based on your ConversationLogic class
+    def update_conversation_text(self):
+        """Updates the conversation text in the GUI based on the loaded conversation from a file"""
+
+        conversation = self.conversational_logic.load_conversation(self.conversational_logic.filename)  # loads the current file (and filename) being used
         messages = conversation.get('messages', [])
 
         self.conversation_text.delete(1.0, tk.END)
 
-        for message in messages[2:]:  # Assuming you want to skip the system and user messages
+        for message in messages[2:]:  # Assuming you want to skip the system and user messages to be displayed in the text-field
             role = message["role"]
             content = message["content"]
             self.conversation_text.insert(tk.END, f"{role.capitalize()}: {content}\n")
@@ -85,18 +117,24 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
         # Implement the logic for creating a new conversation in ConversationLogic
 
     def load_conversation(self):
-        filename = filedialog.askopenfilename( 
-            initialdir="data/conversation",
+        """ Extremely important loading function.
+        
+        filedialog loads a window to choose from existing .json files, thus returning a filename when chosen
+        If the loaded filename is different from the default filename (which is set to data/conversation.jon), the filename is changed in real time """
+
+        filename = filedialog.askopenfilename(  
+            initialdir="data/conversation", # where to open to 
             title="Open Conversation",
-            filetypes=(("JSON files", "*.json"), ("All files", "*.*"))
+            filetypes=(("JSON files", "*.json"), ("All files", "*.*")) 
         )
-        if filename:
-            self.conversational_logic.filename = filename
-            self.conversational_logic.load_conversation(filename)
-            self.update_conversation_text(filename)
+        if filename: 
+            self.conversational_logic.filename = filename # set the given filename to the filename in ConversationLogic() class to dynamically change filenames.
+            self.conversational_logic.load_conversation(filename) # load the given file's conversation
+            self.update_conversation_text() # display the conversation in the gui.
 
     def save_conversation(self):
-        # You may want to ask the user for a filename or use a default one
+        """Opens a window to "Save As" the current conversation to a JSON file."""
+
         filename = filedialog.asksaveasfilename(
             initialdir="data/conversation",
             title="Save Conversation",
@@ -106,12 +144,16 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
             if not filename.endswith(".json"):
                 filename += ".json"
 
-            current_messages = self.conversational_logic.load_conversation().get('messages', [])
+            current_messages = self.conversational_logic.load_conversation().get('messages', []) # Methods used to save the conversation. Load the current conversation, and save it to file. 
             self.conversational_logic.save_conversation_to_file(filename, current_messages)
             messagebox.showinfo("Save", "The conversation has been saved.")
 
     def open_settings_menu(self):
-        settings_window = tk.Toplevel(self.parent) # This creates a sub-window to pop up. Attaches it to the main gui. 
+        """Opens the settings window for configuring ChatGPT settings
+        
+        Apply button applies the changes in real time through the update_settings() method in ConversationLogic()"""
+
+        settings_window = tk.Toplevel(self.parent) # This creates a sub-window to pop up.
         settings_window.geometry('400x300')
         settings_window.title('Settings') 
 
@@ -122,6 +164,7 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
             configs['model'] = model_var.get()
             configs['max_tokens'] = max_tokens_var.get()
             configs['system_message'] = system_message_var.get()
+            configs['OPENAI_API_KEY'] = api_key_var.get()
 
             # ... Update other settings here
 
@@ -129,9 +172,8 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
             with open('configs.json', 'w') as f:
                 json.dump(configs, f)
 
-            self.conversational_logic.update_settings(configs) # when apply is pressed, the changes made are applied in real-time by updating configs in conversation_logic. 
-
-            settings_window.destroy()  #Disable this if you want to close the settings menu open after apply is presed. Otherwise, add a pop-up or notification that says "Settings Changed"
+            self.conversational_logic.update_settings(configs) # updates settings in real-time
+            settings_window.destroy()  #Disable this if you want to close the settings menu open after apply is pressed. Otherwise, add an alert that says "Settings Changed"
 
         # Apply button
         apply_button = tk.Button(settings_window, text='Apply', command=update_config) 
@@ -141,7 +183,7 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
         tk.Label(settings_window, text='Model:').grid(row=0, column=0)
         model_var = tk.StringVar() # creates a selection of String data to get/set. 
         model_select = ttk.Combobox(settings_window, textvariable=model_var) # Dropdown menu for changing models. Text is held in model_var
-        model_select['values'] = ('gpt-3.5-turbo', 'gpt-4-1106-preview', 'gpt-4')  # Set available models here 
+        model_select['values'] = ('gpt-3.5-turbo', 'gpt-3.5-turbo-1106', 'gpt-4-1106-preview', 'gpt-4')  # Set available models here 
         model_select.grid(row=0, column=1) # assigns it to column 1. 
         model_var.set(configs.get('model'))
 
@@ -159,6 +201,12 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
         system_message_entry.grid(row=2, column=1)
         system_message_var.set(configs.get('system_message'))  # Set current system message
 
+        # API key input 
+        tk.Label(settings_window, text='Your API key here:').grid(row=3, column =0)
+        api_key_var = tk.StringVar()
+        api_key_entry = tk.Entry(settings_window, textvariable=api_key_var)
+        api_key_entry.grid(row=3, column=1)
+        api_key_var.set(configs.get('OPENAI_API_KEY'))
 
     def exit_application(self):
         # Save if needed, then exit
@@ -166,7 +214,8 @@ class Main(tk.Frame): # Packs the program in a tkinter frame
         self.parent.quit()
 
 
-if __name__ == "__main__": #main start method 
+if __name__ == "__main__": 
+    """Start method for testing GUI changes"""
     root = tk.Tk()
     root.title("GPT App") # set the name of the app
     #root.geometry('800x600') # sets size of the window 
